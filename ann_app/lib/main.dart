86d3a8f1db04e95eeac4f570ecd07870d63014c8
Charles-Thomson <65188045+Charles-Thomson@users.noreply.github.com>
2023-02-06 16:06:10 +0000

    Agent Rework

--- ann_app/lib/main.dart
+++ ann_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'package:collection/collection.dart';
 
 void main() => runApp(const MyApp());
 
@@ -23,6 +24,11 @@ class CustomScaffold extends StatelessWidget {
       [30.0, 30.0],
       [100.0, 100.0]
     ];
+    List<List<double>> mazeAgentData = <List<double>>[
+      [50.0, 50.0],
+      [20.0, 40.0, 60.0, 200.0, 100.0],
+      [20.0, 40.0, 60.0, 200.0, 100.0]
+    ];
     return Scaffold(
         appBar: AppBar(centerTitle: true, title: const Text("ANN - APP_V2")),
         body: Center(
@@ -30,56 +36,12 @@ class CustomScaffold extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                StackedBoard(obsticalLocationsA: obsticalLocationsA)
+                StackedBoard(
+                  obsticalLocationsA: obsticalLocationsA,
+                  mazeAgentData: mazeAgentData,
+                )
               ]),
         ));
-  }
-}
-
-class MazeAgent extends StatefulWidget {
-  const MazeAgent({super.key});
-
-  @override
-  State<MazeAgent> createState() => _MazeAgent();
-}
-
-class _MazeAgent extends State<MazeAgent> {
-  List growableListY = <double>[50, 300, 300, 50];
-  List growableListX = <double>[50, 50, 300, 300];
-
-  double topPosition = 50;
-  double leftPosiion = 50;
-  int listIndex = 0;
-
-  void changePosition(Timer t) async {
-    listIndex++;
-    (listIndex >= growableListY.length) ? listIndex = 0 : listIndex;
-    (listIndex >= growableListX.length) ? listIndex = 0 : listIndex;
-
-    setState(() {
-      topPosition = growableListY.elementAt(listIndex);
-      leftPosiion = growableListX.elementAt(listIndex);
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    Timer.periodic(const Duration(seconds: 5),
-        changePosition); // Essentially the tick rate
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedPositioned(
-      width: 30,
-      height: 30,
-      top: topPosition,
-      left: leftPosiion,
-      duration: const Duration(seconds: 2), // Move time to new position
-      curve: Curves.fastOutSlowIn,
-      child: Container(color: Colors.blue),
-    );
   }
 }
 
@@ -98,23 +60,112 @@ class MazeObject extends StatelessWidget {
   }
 }
 
-class StackedBoard extends StatelessWidget {
-  final List obsticalLocationsA;
-  StackedBoard({super.key, required this.obsticalLocationsA});
+// Wrking on the passing of agent data to this i.e path and start location
+class MazeAgent extends StatefulWidget {
+  final List<List<double>> agentData;
+  const MazeAgent({super.key, required this.agentData});
+
+  @override
+  State<MazeAgent> createState() => _MazeAgent();
+}
+
+class _MazeAgent extends State<MazeAgent> {
+  late double xLocation;
+  late double yLocation;
+  late List xPathData;
+  late List yPathData;
+
+  double holderT = 0.0;
+  double holderL = 0.0;
+
+  int index = 0;
+
+  // Issue with this - have a nother look at it ?
+  Iterable<List> returnNewPosition() sync* {
+    for (var pair in IterableZip([xPathData, yPathData])) {
+      yield pair;
+    }
+  }
+
+  // New approach just to see if it works
+  List returnNewPositionB() {
+    List returnList = [];
+    returnList.add(xPathData[index]);
+    returnList.add(yPathData[index]);
+    index++;
+    if (index >= xPathData.length) {
+      index = 0;
+    }
+    return returnList;
+  }
+
+  void changePosition(Timer t) {
+    // Iterable<List<dynamic>> newLocation = returnNewPosition();
+    // for (var i in newLocation) {
+    //   holderT = i[0];
+    //   holderL = i[1];
+    // }
+    List newLocation = returnNewPositionB();
+    holderT = newLocation.elementAt(0);
+    holderL = newLocation.elementAt(1);
+
+    setState(() {
+      xLocation = holderT;
+      yLocation = holderL;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    List startLocation = widget.agentData[0]; // Location at index 0
+    yLocation = startLocation.elementAt(0);
+    xLocation = startLocation.elementAt(1);
+
+    xPathData = widget.agentData[1];
+    yPathData = widget.agentData[2];
+
+    Timer.periodic(const Duration(seconds: 5),
+        changePosition); // Essentially the tick rate
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-        children: [const MazeBoard(), const MazeAgent(), ..._getObsticals()]);
+    return AnimatedPositioned(
+      width: 30,
+      height: 30,
+      top: yLocation,
+      left: xLocation,
+      duration: const Duration(seconds: 2), // Move time to new position
+      curve: Curves.fastOutSlowIn,
+      child: Container(color: Colors.blue),
+    );
+  }
+}
+
+class StackedBoard extends StatelessWidget {
+  final List obsticalLocationsA;
+  final List<List<double>> mazeAgentData;
+  StackedBoard(
+      {super.key,
+      required this.obsticalLocationsA,
+      required this.mazeAgentData});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      const MazeBoard(),
+      MazeAgent(agentData: mazeAgentData),
+      ..._getObsticals()
+    ]);
   }
 
-  _getObsticals() {
-    //List obsticals = <Widget>[];
-    // for (var location in obsticalLocationsA) {
-    //   Widget item = MazeObject(location: location);
-    //   obsticals.add(item);
-    // }
+  // _getAgents() {
+  //   var agents = mazeAgentData.map((data) => MazeAgent(agentData: data));
+  //   return agents;
+  //}
 
+  _getObsticals() {
     var obsticals =
         obsticalLocationsA.map((location) => MazeObject(location: location));
 
